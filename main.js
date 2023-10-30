b2d3e1dfa6fa7218f9194eafdf1455e187136fa6
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2023-10-30 20:03:13 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,38 @@ recognition.onresult = function(event)  {
 
     document.getElementById("textbox").innerHTML = Content;
 }
+
+var SpeechRecognition  = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+recognition.onresult = function(event)
+{
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    speak();
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").value;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
